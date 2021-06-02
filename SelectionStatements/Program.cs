a0fa5b0f831db1fa06ce0f181382146c01191d53
mattiyee <mattiyee610@gmail.com>
2021-06-02 19:00:32 -0400

    initial commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var favNumber = random.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("You guessed the right number!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind.");
+            }
+        }
+    }
+}
